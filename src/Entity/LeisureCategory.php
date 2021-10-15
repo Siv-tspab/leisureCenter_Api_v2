ab0bca79889a08118ac999534c5c4e0ec8a0ee32
Siv-tspab <sbapst.j@gmail.com>
2021-10-15 19:24:18 +0200

    EDIT: create groups for read on entity + ADD: coord + weather on center

--- src/Entity/LeisureCategory.php
+++ src/Entity/LeisureCategory.php
@@ -6,6 +6,7 @@ use App\Repository\LeisureCategoryRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use JMS\Serializer\Annotation as Serializer;
 
 /**
  * @ORM\Entity(repositoryClass=LeisureCategoryRepository::class)
@@ -16,11 +17,15 @@ class LeisureCategory
      * @ORM\Id
      * @ORM\GeneratedValue
      * @ORM\Column(type="integer")
+     * 
+     * @Serializer\Groups({"list","detail"})
      */
     private $id;
 
     /**
      * @ORM\Column(type="string", length=255)
+     * 
+     * @Serializer\Groups({"list","detail"})
      */
     private $name;
 
--- src/Entity/LeisureCenter.php
+++ src/Entity/LeisureCenter.php
@@ -6,6 +6,7 @@ use App\Repository\LeisureCenterRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use JMS\Serializer\Annotation as Serializer;
 
 /**
  * @ORM\Entity(repositoryClass=LeisureCenterRepository::class)
@@ -16,31 +17,55 @@ class LeisureCenter
      * @ORM\Id
      * @ORM\GeneratedValue
      * @ORM\Column(type="integer")
+     * 
+     * @Serializer\Groups({"list"})
      */
     private $id;
 
     /**
      * @ORM\Column(type="string", length=255)
+     * 
+     * @Serializer\Groups({"list","detail"})
      */
     private $name;
 
     /**
      * @ORM\Column(type="text", nullable=true)
+     * 
+     * @Serializer\Groups({"list","detail"})
      */
     private $description;
 
     /**
      * @ORM\Column(type="string", length=255)
+     * 
+     * @Serializer\Groups({"list","detail"})
      */
     private $address;
 
     /**
      * @ORM\Column(type="string", length=255, nullable=true)
+     * 
+     * @Serializer\Groups({"list","detail"})
      */
     private $url;
 
     /**
+     * @ORM\Column(type="array")
+     * 
+     * @Serializer\Groups({"list","detail"})
+     */
+    private $coordinates = [];
+
+    /**
+     * @Serializer\Groups({"list","detail"})
+     */
+    private $weather = [];
+
+    /**
      * @ORM\ManyToMany(targetEntity=LeisureCategory::class, inversedBy="leisureCenters")
+     * 
+     * @Serializer\Groups({"list","detail"})
      */
     private $leisureCategory;
 
@@ -98,6 +123,30 @@ class LeisureCenter
     public function setUrl(?string $url): self
     {
         $this->url = $url;
+
+        return $this;
+    }
+
+    public function getCoordinates(): ?array
+    {
+        return $this->coordinates;
+    }
+
+    public function setCoordinates(array $coordinates): self
+    {
+        $this->coordinates = $coordinates;
+
+        return $this;
+    }
+
+    public function getWeather(): ?array
+    {
+        return $this->weather;
+    }
+
+    public function setWeather(?array $weather): self
+    {
+        $this->weather = $weather;
 
         return $this;
     }
